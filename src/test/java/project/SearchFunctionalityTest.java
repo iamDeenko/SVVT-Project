5826d17bf866c1ace87263910274d9dc517b1b7f
iamDeenko <iamdeenko@gmail.com>
2026-01-05 17:21:46 +0100

    Adding one more test

--- src/test/java/project/SearchFunctionalityTest.java
+++ src/test/java/project/SearchFunctionalityTest.java
@@ -26,7 +26,42 @@ public class SearchFunctionalityTest extends BaseTest
         longWait();
 
         assertEquals(expectedURL, webDriver.getCurrentUrl());
+    }
 
+
+    @Test
+    public void testNonExistentItem()
+    {
+        webDriver.get(baseURL);
+
+        acceptCookies();
+
+        WebElement inputField = webDriver.findElement(By.xpath("/html/body/div[1]/div/div[1]/header/div[1]/div[2]/div/div/div[1]/form/input"));
+        inputField.sendKeys("h34haa34h0" + Keys.ENTER);
+
+        WebElement notFoundH2 = webDriver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div[3]/div[2]/section[1]/div[1]/h2"));
+        String notFoundText = notFoundH2.getText();
+
+        assertEquals("NAŽALOST NISMO USPJELI PRONAĆI: H34HAA34H0", notFoundText);
+    }
+
+
+    @Test
+    public void testSqlInjectionProof() throws InterruptedException
+    {
+        webDriver.get(baseURL);
+
+        acceptCookies();
+
+        WebElement inputField = webDriver.findElement(By.xpath("/html/body/div[1]/div/div[1]/header/div[1]/div[2]/div/div/div[1]/form/input"));
+        inputField.sendKeys("SELECT * FROM USERS" + Keys.ENTER);
+
+        shortWait();
+
+        WebElement accessDeniedH2 = webDriver.findElement(By.xpath("/html/body/h1"));
+        String accessDeniedText = accessDeniedH2.getText();
+
+        assertEquals("Access Denied", accessDeniedText);
     }
 
 }
