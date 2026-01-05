11ac6573f5f19aebfd8be84c0904d5b2b82de5a5
iamDeenko <iamdeenko@gmail.com>
2026-01-05 15:45:18 +0100

    Adding more tests, and fixing some bugs

--- src/test/java/project/NewsletterValidationTest.java
+++ src/test/java/project/NewsletterValidationTest.java
@@ -20,9 +20,7 @@ public class NewsletterValidationTest extends BaseTest
         // Open the browser window
         webDriver.get(baseURL);
 
-
         acceptCookies();
-
 
         // Scroll to the name input field and input our name
         WebElement nameInputField = webDriver.findElement(By.id("newsletter-firstname"));
@@ -44,14 +42,12 @@ public class NewsletterValidationTest extends BaseTest
         // Continue to send the newsletter?
         webDriver.findElement(By.id("edit-terms")).click();
 
-
         // If the email is incorrect a little notification saying that we need a correct email address will pop up below the input field
-        WebElement webElement = webDriver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div[5]/div/div/form/div[1]/div[2]/div[2]"));
+        WebElement invalidEmailDiv = webDriver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div[5]/div/div/form/div[1]/div[2]/div[2]"));
 
         // Get the value from the webelement and compare it. It should be "Molimo unesite važeću E-mail adresu.".
-        String value = webElement.getText();
-        String expectedValue = "Molimo unesite važeću E-mail adresu.";
-        assertEquals(expectedValue, value);
+        String invalidEmailText = invalidEmailDiv.getText();
+        assertEquals("\"Molimo unesite važeću E-mail adresu.\"", invalidEmailText);
     }
 
 
@@ -62,10 +58,7 @@ public class NewsletterValidationTest extends BaseTest
         // Open the browser window
         webDriver.get(baseURL);
 
-
-
         acceptCookies();
-
 
         // Scroll to the name input field and input our name
         WebElement nameInputField = webDriver.findElement(By.id("newsletter-firstname"));
@@ -74,7 +67,7 @@ public class NewsletterValidationTest extends BaseTest
 
         shortWait();
 
-        //Input an invalid email
+        //Input an email
         webDriver.findElement(By.id("teradata-email")).sendKeys("test@gmail.com");
 
         shortWait();
@@ -84,16 +77,14 @@ public class NewsletterValidationTest extends BaseTest
 
         shortWait();
 
-        // Continue to send the newsletter?
+        // Subscribe to the newsletter.
         webDriver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div[5]/div/div/form/div[1]/div[3]/button")).click();
 
-
         // If the email is incorrect a little notification saying that we need a correct email address will pop up below the input field
-        WebElement webElement = webDriver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div[5]/div/div/form/div[1]/h2"));
+        WebElement subsriptionDiv = webDriver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div[5]/div/div/form/div[1]/h2"));
 
         // Get the value from the webelement and compare it. It should be "Hvala vam na zanimanju za naše Novosti.".
-        String value = webElement.getText();
-        String expectedValue = "Hvala vam na zanimanju za naše Novosti.";
-        assertEquals(expectedValue, value);
+        String subscriptionText = subsriptionDiv.getText();
+        assertEquals("Hvala vam na zanimanju za naše Novosti.", subscriptionText);
     }
 }
